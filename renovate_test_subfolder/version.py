24202e8936e80903235c4a23d124869095d23b32
ITimLindnerI <60129741+ITimLindnerI@users.noreply.github.com>
2023-09-20 13:37:25 +0200

    Create version.py

---
+++ renovate_test_subfolder/version.py
@@ -0,0 +1,6 @@
+"""Version and other package information constants."""
+
+__some_stuff_also = 'testvariable'
+__version_info__ = (0, 1, 1)
+__version__ = '.'.join(map(str, __version_info__))
+__some_stuff = 'test'
